260f8bf52c94125d3e9594b75290e5efd5b92f39
Jäger <69374608+jager-js@users.noreply.github.com>
2020-08-09 07:52:27 -0500

    Create quik-linux.sh

---
+++ quik-linux.sh
@@ -0,0 +1,10 @@
+echo "Prepare to see some magic before your very eyes."
+
+git clone https://github.com/vinceliuice/Matcha-gtk-theme
+cd Matcha-gtk-theme
+./install.sh
+pacman -S papirus-icon-theme
+pacman -S discord
+pacman -S chromium
+
+echo "Bye bye."
